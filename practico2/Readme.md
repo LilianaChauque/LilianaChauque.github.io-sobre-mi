9fa5e364a041de22a42ef2e8ba76390acb8260cb
Liliana Chauque <lilianachauque27@hotmail.com>
2023-09-01 22:15:07 -0300

    Parte 3: Selección de la opción del jugador y del oponente (PC)

--- practico2/Readme.md
+++ practico2/Readme.md
@@ -8,3 +8,9 @@
  * `main.js` archivos para las acciones dinamicas de la app:
 
      * `agregarNombre()`: funcion para tomar el nombre del usuario. Esta vinculada a un evento de tipo _onclick_ en el input con _id="nombre"_. Se valida que el mismo no este vacio, también se realiza un saludo personalizado al jugador.
+
+#  PARTE 3: Selección de la opción del jugador y del oponente (PC) 
+ * Cree una carpeta en  `index.html` llamada _imagenes_ donde se encuentran las imagenes utilizadas para identificar las opciones que tiene el jugador: _Piedra, Papel o Tijera_.
+ * En `styles.css` le di estilo a esas imagenes.
+ * Pude capturar la opción seleccionada por el jugador y también la opción aleatoria de la PC, todo esto en `script.js`.
+ 
--- practico2/script.js
+++ practico2/script.js
@@ -4,11 +4,54 @@ let $nombreInput = document.getElementById("nombre");
 
 function agregarNombre() {
     let nombre = $nombreInput.value;
-    if (nombre === "") { //Valida que el campo no este vacio.
-        alert("Por favor, escriba su nombre.");
+    if (nombre === "") {  //Valida que el campo no este vacio.
+       alert("Por favor, escriba su nombre, para empezar a jugar.");
     }
     else{  
-    alert("Hola, " + nombre +"!!!");//Saludo personalizado para el jugador.
-   
+       alert("Hola, " + nombre +"!!!");//Saludo personalizado para el jugador.
+       return;
     }
+    
 }
+// declaramos las variables
+    let $piedraButton = document.getElementById("piedra");
+    let $papelButton = document.getElementById("papel");
+    let $tijeraButton = document.getElementById("tijera");
+    let jugadaUsuario;
+    let jugadaCompu;
+    
+/* obtenemos la jugada del usuario cuando hacemos click en la imagen, 
+a su vez llamamos a la jugada de la PC, invocando la función  obtenerJugadaComputadora() */
+
+$piedraButton.addEventListener("click", () =>{
+    jugadaUsuario = "piedra";
+    console.log(jugadaUsuario); // Utilizo console.log para ver si funciona.
+    obtenerJugadaComputadora();
+    console.log(obtenerJugadaComputadora());
+});
+
+$papelButton.addEventListener("click", () =>{
+    jugadaUsuario = "papel";
+    console.log(jugadaUsuario);
+    obtenerJugadaComputadora();
+    console.log(obtenerJugadaComputadora());
+});
+
+$tijeraButton.addEventListener("click", () =>{
+    jugadaUsuario = "tijera";
+    console.log(jugadaUsuario);
+    obtenerJugadaComputadora();
+    console.log(obtenerJugadaComputadora());
+});
+
+ // Obtenemos la jugada de la computadora de manera aleatoria
+ function obtenerJugadaComputadora() {
+     let jugada = ["piedra", "papel", "tijera"]
+     let jugadaCompu = (jugada[Math.floor(Math.random()*3)]);  
+ 
+     return jugadaCompu;  
+    
+ }
+ 
+ 
+ 
