566240315d4094a49b8168a8eb1afedf31b8ed12
Liliana Chauque <lilianachauque27@hotmail.com>
2023-09-05 15:47:32 -0300

    Determinación del ganador y actualización del marcador

--- practico2/Readme.md
+++ practico2/Readme.md
@@ -4,13 +4,17 @@
  * `index.html` este es el archivo base para todo el proyecto.
 
  * `styles.css` archivo para dar estilos generales.
-# PARTE 2: Obtención del nombre del jugador
- * `main.js` archivos para las acciones dinamicas de la app:
+## PARTE 2: Obtención del nombre del jugador
+ * `script.js` archivos para las acciones dinamicas de la app:
 
      * `agregarNombre()`: funcion para tomar el nombre del usuario. Esta vinculada a un evento de tipo _onclick_ en el input con _id="nombre"_. Se valida que el mismo no este vacio, también se realiza un saludo personalizado al jugador.
 
-#  PARTE 3: Selección de la opción del jugador y del oponente (PC) 
+## PARTE 3: Selección de la opción del jugador y del oponente (PC) 
  * Cree una carpeta en  `index.html` llamada _imagenes_ donde se encuentran las imagenes utilizadas para identificar las opciones que tiene el jugador: _Piedra, Papel o Tijera_.
  * En `styles.css` le di estilo a esas imagenes.
  * Pude capturar la opción seleccionada por el jugador y también la opción aleatoria de la PC, todo esto en `script.js`.
  
+## PARTE 4: Determinación del ganador y actualización del marcador
+ *  En `index.html` Establecí las reglas del juego, con _p_ en un parrafo, tambien agregue un _div_ en el cual se encuentra el marcador del juego, donde se va sumando la cantidad de puntos que obtienen los jugadores (usuario y PC).
+ * En `styles.css`le di estilo a lo que incorporamos en `index.html`.
+ * En  `script.js` se determina el ganador de las partidas que se juegan y se muestra por pantalla quien la gano y que eligio la PC. Todo esto con las funciones que se añadieron y algunas modificaciones en otras.
--- practico2/script.js
+++ practico2/script.js
@@ -1,5 +1,5 @@
 let $nombreInput = document.getElementById("nombre");
-
+let saludoNombre = document.getElementById("aJugar");
 // Función que agrega el nombre del jugador.
 
 function agregarNombre() {
@@ -8,50 +8,89 @@ function agregarNombre() {
        alert("Por favor, escriba su nombre, para empezar a jugar.");
     }
     else{  
-       alert("Hola, " + nombre +"!!!");//Saludo personalizado para el jugador.
+      alert("Hola, " + nombre +"!!!");//Saludo personalizado para el jugador.
        return;
     }
-    
+       
 }
 // declaramos las variables
     let $piedraButton = document.getElementById("piedra");
-    let $papelButton = document.getElementById("papel");
+    let $papelButton = document.getElementById("papel");    
     let $tijeraButton = document.getElementById("tijera");
+    let msjganador = document.getElementById("rdoJugada");
+    let marcadorPuntoUsuario = document.getElementById("puntoUsuario");
+    let marcadorPuntoCompu = document.getElementById("puntoCompu");
+    let puntoUsuario = 0;
+    let puntoCompu = 0;
     let jugadaUsuario;
     let jugadaCompu;
-    
-/* obtenemos la jugada del usuario cuando hacemos click en la imagen, 
-a su vez llamamos a la jugada de la PC, invocando la función  obtenerJugadaComputadora() */
+    let resultado;
+           
 
-$piedraButton.addEventListener("click", () =>{
+/* obtenemos la jugada del usuario cuando hacemos click en la imagen, 
+a su vez llamamos a la jugada de la PC, invocando la función  obtenerJugadaComputadora()*/
+
+
+$piedraButton.addEventListener("click",  () =>{
     jugadaUsuario = "piedra";
     console.log(jugadaUsuario); // Utilizo console.log para ver si funciona.
     obtenerJugadaComputadora();
-    console.log(obtenerJugadaComputadora());
+    
+   
 });
 
-$papelButton.addEventListener("click", () =>{
+$papelButton.addEventListener("click", ()=>{
     jugadaUsuario = "papel";
     console.log(jugadaUsuario);
     obtenerJugadaComputadora();
-    console.log(obtenerJugadaComputadora());
+   
+   
 });
 
-$tijeraButton.addEventListener("click", () =>{
+$tijeraButton.addEventListener("click", ()=>{
     jugadaUsuario = "tijera";
     console.log(jugadaUsuario);
     obtenerJugadaComputadora();
-    console.log(obtenerJugadaComputadora());
+    
+   
 });
+
+
 
  // Obtenemos la jugada de la computadora de manera aleatoria
  function obtenerJugadaComputadora() {
      let jugada = ["piedra", "papel", "tijera"]
-     let jugadaCompu = (jugada[Math.floor(Math.random()*3)]);  
- 
-     return jugadaCompu;  
+     let nRandon = ([Math.floor(Math.random()*3)]); 
+     jugadaCompu = jugada[nRandon];
+     console.log ("jugada de la computadora " + jugadaCompu);
+     determinarGanador();
     
  }
- 
- 
- 
+ //definimos una función para saber quien gana la partida, mostrando el rdo con innerHTML.
+ function determinarGanador(){
+    if (jugadaUsuario === jugadaCompu){
+        msjganador.innerHTML= "Uh, empate! 😐  ambos eligieron " + jugadaUsuario;  
+    }
+     else if ((jugadaUsuario === "piedra" && jugadaCompu === "tijera") ||
+        (jugadaUsuario === "papel" && jugadaCompu === "piedra") || 
+        (jugadaUsuario === "tijera" && jugadaCompu === "papel") ){
+         msjganador.innerHTML="Ganaste!!! 😆 la PC eligio " + jugadaCompu;
+       ganaUsuario();   
+    }
+     else{
+        msjganador.innerHTML="Gano la PC... 😠 eligio " + jugadaCompu;
+        ganaCompu()
+     }
+ }
+ // defino función para el marcador, así se puede observar por monitor cuantos puntos tiene cada uno.
+function ganaCompu(){    
+    puntoCompu++;   
+    marcadorPuntoCompu.innerHTML = puntoCompu;
+    
+}
+    
+function ganaUsuario(){
+    puntoUsuario++; 
+    marcadorPuntoUsuario.innerHTML = puntoUsuario;   
+   
+}
